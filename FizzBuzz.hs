98d209e542ee329e3ea4e9e526130f769d95acf3
jebax <joel.eliot.baxter@gmail.com>
2018-11-01 14:02:05 +0000

    refactored to use divisbleBy function

--- FizzBuzz.hs
+++ FizzBuzz.hs
@@ -1,4 +1,12 @@
 module FizzBuzz where
 
 fizzBuzz :: Int -> String
-fizzBuzz n = if n == 3 then "Fizz" else if n == 5 then "Buzz" else "FizzBuzz"
+fizzBuzz n = if divisibleBy n 15
+                then "FizzBuzz"
+                else if divisibleBy n 5
+                    then "Buzz"
+                    else "Fizz"
+
+
+divisibleBy :: Int -> Int -> Bool
+divisibleBy num divisor = num `mod` divisor == 0
